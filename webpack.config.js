c072d0053c59711b53045f7b7d8db9cf1f307fd3
Matthew Robb <mr.obb@outlook.com>
2018-12-16 02:52:34 -0500

    should be working now

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path = require('path');
 
-module.exports = {
-  entry: './snips/mithril-mobx/index.js',
-  mode: 'none',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './public/mithril-mobx')
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.(html)$/,
-        use: ['file-loader?name=[name].[ext]', 'extract-loader', 'html-loader']
-      },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
-  }
-};
+function snip(name) {
+  return {
+    entry: `./src/${name}/index.js`,
+    mode: 'none',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, `./public/${name}`)
+    },
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          use: ['file-loader?name=[name].[ext]', 'extract-loader', 'html-loader']
+        },
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      ]
+    }
+  };
+}
+
+module.exports = [
+  snip("mithril-mobx")
+];
